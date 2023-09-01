2284098dc7b7e22281671d994dc6b955e0d415f4
Mert Aygün <mertaygun1923@gmail.com>
2023-09-01 13:32:55 +0300

    Add project files.

---
+++ personManager.webapp/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using personManager.webapp.Repostory;
+
+namespace personManager.webapp.Controllers
+{
+    public class PersonController : Controller
+    {
+        public readonly PersonRepository _personRepository;
+        public PersonController()
+        {
+            _personRepository = new PersonRepository();
+        }
+        public IActionResult GetAll()
+        {
+            var persons = _personRepository.GetAll();
+            return View(persons);
+        }
+
+        public IActionResult GetById(int id) 
+        {
+            var person = _personRepository.GetById(id);
+            return View(person);
+        }
+
+    }
+}
---
+++ personManager.webapp/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace personManager.webapp.Models;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+}
